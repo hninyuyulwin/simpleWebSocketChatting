821b0aa9294936282e28ffda397ca7881389b726
Rosie Mary <laltnadi@gmail.com>
2022-09-26 21:19:27 +0630

    testing simple web socket chatting

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let express = require("express");
+let socket = require('socket.io');
+
+//app setup
+let app = express();
+
+//server setup
+let server = app.listen(4000, () => {
+  console.log("Project is running on localhost:4000");
+});
+
+//rotue setup
+app.get('/', (res, req) => {
+  req.sendFile(__dirname + '/public/index.html');
+});
+
+//socket setup
+let io = socket(server);
+io.on('connection', (socket) => {
+  //console.log('Socket connection connected!'+socket.id);
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  });
+  socket.on('typing', (name) => {
+    socket.broadcast.emit('typing', name) ;
+  });
+});
